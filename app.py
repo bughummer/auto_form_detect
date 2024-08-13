41a436b7618d260119178f55218d26cb28b02832
bughummer <agamir.agazade@gmail.com>
2024-08-13 22:10:05 +0400

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pandas as pd
 import torch
 import plotly.graph_objs as go
 from sklearn.preprocessing import MinMaxScaler
+import numpy as np
+
 
 # Define the LSTM model class
 class LSTMModel(torch.nn.Module):
