c572181dc0a3088fbde8f7b40dde2d7a84234cbf
bughummer <agamir.agazade@gmail.com>
2024-08-14 19:28:38 +0400

    subplot width

--- app.py
+++ app.py
@@ -70,7 +70,8 @@ def main(df, selected_wells, look_back=50, mean_multiplier=0.5, merge_threshold=
      column_widths = [0.25] * num_wells
     else:
      column_widths = [1.0 / num_wells] * num_wells
-
+    st.write(num_wells)
+    st.write(column_widths)
     # Create subplots with one column per well, and shared y-axis for depth
     fig = make_subplots(rows=1, cols=num_wells, shared_yaxes=True, subplot_titles=selected_wells, column_widths=column_widths)
 
